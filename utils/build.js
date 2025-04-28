07925bf54955c85d81938553c6d059dcefef26f3
Zelchi <tininhakawaii@gmail.com>
2025-04-28 14:55:32 -0300

    init: setup initial project and Hello World!

---
+++ utils/build.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
+const htmlPath = path.join('./', 'index.html');
+const cssPath = path.join('styles', 'index.css');
+const jsPath = path.join('scripts', 'index.js');
+const outputPath = path.join('dist', 'index.html');
+
+const html = fs.readFileSync(htmlPath, 'utf-8');
+const css = fs.readFileSync(cssPath, 'utf-8');
+const js = fs.readFileSync(jsPath, 'utf-8');
+
+const combined = html
+.replace('</head>', `<style>${css}</style></head>`)
+.replace('</body>', `<script>${js}</script></body>`);
+
+fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+fs.writeFileSync(outputPath, combined, 'utf-8');
+
+console.log('Build concluído: dist/index.html');
