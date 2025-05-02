a9373b2ea961cacd71e87310cb96f45f36051bf5
Zelchi <tininhakawaii@gmail.com>
2025-05-02 15:52:12 -0300

    style: Refactor footer styles for improved layout and alignment

--- src/scripts/subscribre.js
+++ src/scripts/subscribre.js
@@ -9,41 +9,44 @@ inputBox.appendChild(message);
 
 const savedEmail = localStorage.getItem('email');
 if (savedEmail) {
-	message.textContent = 'You are already subscribed!';
-	message.style.color = 'orange';
-	buttonText.textContent = 'Unsubscribe';
-	input.classList.add('hidden');
-	button.classList.add('unsubscribre__button');
-	message.classList.add('unsubscribre__message');
+    message.textContent = 'You are already subscribed!';
+    message.style.color = '#FFAD3B';
+    buttonText.textContent = 'Unsubscribe';
+    input.classList.add('hidden'); 
+    button.classList.add('unsubscribre__button'); 
+    message.classList.add('unsubscribre__message');
 }
 
 button.addEventListener('click', (e) => {
-	e.preventDefault();
+    e.preventDefault();
 
-	const inputValue = input.value.trim();
-	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const inputValue = input.value.trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-	if (buttonText.textContent === 'Unsubscribe') {
-		localStorage.removeItem('email');
-		message.textContent = 'You have unsubscribed successfully.';
-		buttonText.textContent = 'Subscribe';
-		input.value = '';
-		input.classList.remove('hidden');
-		button.classList.remove('unsubscribre__button');
-		message.classList.remove('unsubscribre__message');
-	} else if (emailRegex.test(inputValue)) {
-		if (savedEmail === inputValue) {
-			message.textContent = 'You are already subscribed!';
-		} else {
-			localStorage.setItem('email', inputValue);
-			message.textContent = 'Email saved successfully!';
-			buttonText.textContent = 'Unsubscribe';
-			input.value = '';
-			input.classList.add('hidden');
-			button.classList.add('unsubscribre__button');
-		}
-	} else {
-		message.textContent = 'Oops! The email address you entered is invalid.';
-		message.style.color = 'red';
-	}
+    if (buttonText.textContent === 'Unsubscribe') {
+        localStorage.removeItem('email');
+        message.textContent = 'You have unsubscribed successfully.';
+        message.style.color = '#FFAD3B';
+        buttonText.textContent = 'Subscribe';
+        input.value = '';
+        input.classList.remove('hidden'); 
+        button.classList.remove('unsubscribre__button');
+        message.classList.remove('unsubscribre__message');
+    } else if (emailRegex.test(inputValue)) {
+        if (savedEmail === inputValue) {
+            message.textContent = 'You are already subscribed!';
+            message.style.color = '#FFAD3B';
+        } else {
+            localStorage.setItem('email', inputValue);
+            message.textContent = 'Email saved successfully!';
+            message.style.color = '#FFAD3B';
+            buttonText.textContent = 'Unsubscribe';
+            input.value = '';
+            input.classList.add('hidden'); 
+            button.classList.add('unsubscribre__button');
+        }
+    } else {
+        message.textContent = 'Oops! The email address you entered is invalid.';
+        message.style.color = 'red';
+    }
 });
