140960f9c47d3d4bc8d277712fcef124f608d307
Zelchi <tininhakawaii@gmail.com>
2025-05-02 15:03:37 -0300

    feat: Refactor subscription input structure and enhance functionality with unsubscribe option

--- src/scripts/subscribre.js
+++ src/scripts/subscribre.js
@@ -1,17 +1,20 @@
 const inputBox = document.querySelector('.subscribre__input__box');
-const input = inputBox.querySelector('#subscribre__input');
-const button = inputBox.querySelector('#subscribre__button');
+const input = inputBox.querySelector('.subscribre__input');
+const button = inputBox.querySelector('.subscribre__button');
 const buttonText = button.querySelector('p');
 
 const message = document.createElement('p');
 message.classList.add('subscribre__message');
-inputBox.insertAdjacentElement('afterend', message);
+inputBox.appendChild(message);
 
 const savedEmail = localStorage.getItem('email');
 if (savedEmail) {
 	message.textContent = 'You are already subscribed!';
 	message.style.color = 'orange';
 	buttonText.textContent = 'Unsubscribe';
+	input.classList.add('hidden');
+	button.classList.add('unsubscribre__button');
+	message.classList.add('unsubscribre__message');
 }
 
 button.addEventListener('click', (e) => {
@@ -23,19 +26,21 @@ button.addEventListener('click', (e) => {
 	if (buttonText.textContent === 'Unsubscribe') {
 		localStorage.removeItem('email');
 		message.textContent = 'You have unsubscribed successfully.';
-		message.style.color = 'red';
 		buttonText.textContent = 'Subscribe';
 		input.value = '';
+		input.classList.remove('hidden');
+		button.classList.remove('unsubscribre__button');
+		message.classList.remove('unsubscribre__message');
 	} else if (emailRegex.test(inputValue)) {
 		if (savedEmail === inputValue) {
 			message.textContent = 'You are already subscribed!';
-			message.style.color = 'orange';
 		} else {
 			localStorage.setItem('email', inputValue);
 			message.textContent = 'Email saved successfully!';
-			message.style.color = 'green';
 			buttonText.textContent = 'Unsubscribe';
 			input.value = '';
+			input.classList.add('hidden');
+			button.classList.add('unsubscribre__button');
 		}
 	} else {
 		message.textContent = 'Oops! The email address you entered is invalid.';
