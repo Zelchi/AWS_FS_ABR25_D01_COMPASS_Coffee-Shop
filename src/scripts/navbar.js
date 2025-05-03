acaa7d9dd7a4688b71c86d02d7f4ce42d97b9148
Zelchi <tininhakawaii@gmail.com>
2025-05-03 01:34:52 -0300

    style: Adjust CSS formatting for consistency and improve mobile menu styles

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -2,11 +2,11 @@ const hamburgerMenu = document.querySelector('.navbar__hamburger__menu');
 const mobileMenu = document.querySelector('.navbar__mobile__menu');
 
 hamburgerMenu.addEventListener('click', () => {
-    if (mobileMenu.classList.contains('mobile__menu__open')) {
-        mobileMenu.classList.remove('mobile__menu__open');
-        setTimeout(() => mobileMenu.classList.add('hidden'), 500);
-    } else {
-        mobileMenu.classList.remove('hidden');
-        setTimeout(() => mobileMenu.classList.add('mobile__menu__open'), 10);
-    }
+	if (mobileMenu.classList.contains('mobile__menu__open')) {
+		mobileMenu.classList.remove('mobile__menu__open');
+		setTimeout(() => mobileMenu.classList.add('hidden'), 500);
+	} else {
+		mobileMenu.classList.remove('hidden');
+		setTimeout(() => mobileMenu.classList.add('mobile__menu__open'), 10);
+	}
 });
